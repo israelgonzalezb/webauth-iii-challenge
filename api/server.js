0d4fc2c86374fbf5c9a7e659154c870d1e399fe3
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-08-27 19:27:59 -0700

    initial commit, adding node and express boilerplate

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const authRouter = require("..auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+
+const server = express();
+
+const middlewares = [helmet(), express.json(), cors()]
+
+server.use(middlewares);
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
+server.get("/", (req, res)=>{
+    res.send("The server is running");
+})
+
+mmodule.exports = server;
